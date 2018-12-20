371eb496e68ff34084d439a92abeed236f020331
citiq <banelem@citiqprepaid.com>
2018-12-20 13:27:44 +0200

    Added a GPS coordinates output. It works well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,10 +28,14 @@ export class AppComponent {
           console.log(place);
           if (place.geometry === undefined || place.geometry === null ) {
             // return;
-            console.log('Place unknown!!');
+            console.log('Sorry address not found. Please enter manually!');
           } else {
             console.log('Search value: ' + place.formatted_address);
             console.log('Address components: ' + place.address_components);
+            console.log('Geometry location: ' + place.geometry.location);
+            // for(let a = 0; a < place.address_components.length; a++) {
+            //   console.log('Value ' + a + ': ' + place.address_components);
+            // }
           }
         });
       });
@@ -39,7 +43,3 @@ export class AppComponent {
   }
 
 }
-/*
-name: "29 Indlovu St"
-vicinity: "Mfuleni"
-*/
